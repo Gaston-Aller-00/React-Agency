068da96f1ccf6c21f81b6973fa2d3f75bfb83741
Gaston <gastonallerdev@gmail.com>
2024-01-02 01:10:27 -0300

    NavBar principio

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
-type Props = {}
+type Props = {
+  text: string;
+};
 
-const Navbar = (props: Props) => {
+const Navbar = ({ text }: Props) => {
+  const [isMenuopen, setIsMenuOpen] = useState<boolean>(false);
+  const [isSticky, setIsSticky] = useState<boolean>(false);
+
+  //set toggle menu
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuopen)
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.addEventListener("scroll", handleScroll);
+    };
+  },[]);
+
   return (
-    <div>Navbar</div>
-  )
-}
+  <div>
+    Navbar {text}
+    </div>)
+};
+
+export default Navbar;

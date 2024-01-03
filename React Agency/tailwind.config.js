dd3f5f3149c934ab9e18944a743a68e339959db1
Gaston <gastonallerdev@gmail.com>
2024-01-03 01:26:38 -0300

    add

---
+++ React Agency/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "brandPrimary": "#4CAF4F",
+        "neutralDGrey": "#4D4D4D",
+        "neutralGrey": "#717171",
+        "neutralSilver": "#F5F7FA",
+        "gray900": "#18191F",
+        "neutralBlack": "#263238",
+      }
+    },
+  },
+  plugins: [],
+}

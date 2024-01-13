137592cf2d6df0663e1a780a86e455fcecf221be
Gaston <gastonallerdev@gmail.com>
2024-01-13 11:52:52 -0300

    App

--- React Agency/src/App.jsx
+++ React Agency/src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
+import About from "./components/About";
+import Blog from "./components/Blog";
 import Home from "./components/Home";
+import MyFooter from "./components/MyFooter";
 import NavBar from "./components/NavBar";
+import Newsletter from "./components/Newsletter";
+import Products from "./components/Products";
 import Services from "./components/Services";
 
 function App() {
@@ -9,6 +14,11 @@ function App() {
       <NavBar />
       <Home/>
       <Services/>
+      <About/>
+      <Products/>
+      <Blog/>
+      <Newsletter/>
+      <MyFooter/>
     </>
   );
 }
